6f19317bca0325fc7fa1785a128d93dcc548c4f6
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-06-16 19:50:29 +0700

    a

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// Mendapatkan elemen dengan class "addToCartBtn"
+var addToCartButtons = document.getElementsByClassName("addToCartBtn");
+
+// Fungsi untuk menampilkan pop-up
+function showPopup() {
+    alert("Product has been added to cart.");
+}
+
+// Menambahkan event listener untuk setiap tombol "Add to cart"
+for (var i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", showPopup);
+}
+
+// Mendapatkan elemen dengan class "notAvailableBtn"
+var notAvailableButtons = document.getElementsByClassName("notAvailableBtn");
+
+// Fungsi untuk menampilkan pop-up
+function showNotAvailablePopup() {
+    alert("Fitur belum tersedia.");
+}
+
+// Menambahkan event listener untuk setiap tombol "Not Available"
+for (var i = 0; i < notAvailableButtons.length; i++) {
+    notAvailableButtons[i].addEventListener("click", showNotAvailablePopup);
+}
